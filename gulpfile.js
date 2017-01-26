33f6349c0ae46b32d4aa599e8b6378d960506b2d
Daniel Eytan Schneider <Daniel@Daniels-MacBook-Pro.local>
2017-01-26 20:51:49 +0100

    Update Fancy Set-Up
    * Watch JSON Error

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var clean = require('gulp-clean');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+
+
+ gulp.task('clean', function () {
+    return gulp.src('./dist/*', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('templates', function () {
+    'use strict';
+    var twig = require('gulp-twig');
+    var contentde = require('./content/de.json');
+    return gulp.src('./src/templates/**/*.twig')
+        .pipe(twig({
+            data: contentde
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('templates:watch', function () {
+  return gulp.watch('./src/templates/**/*.twig', gulp.series('templates'));
+});
+ 
+gulp.task('css', function () {
+  return gulp.src('./src/css/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('css:watch', function () {
+  return gulp.watch('./src/css/**/*.scss', gulp.series('css'));
+});
+
+gulp.task('content:watch', function () {
+  return gulp.watch('./content/*.json', gulp.series('templates'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
+});
+
+gulp.task('default', gulp.series('clean', gulp.parallel('browser-sync', 'templates', 'css', 'css:watch', 'templates:watch', 'content:watch')));
+
+
+ 
